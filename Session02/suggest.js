d6f341aab1bb6fb30f802ab135fd68fe835d8ea4
longpham776 <lpham776@gmail.com>
2022-04-16 11:59:15 +0700

    Push Session02

--- Session02/suggest.js
+++ Session02/suggest.js
@@ -1,39 +1,40 @@
-let cover = document.createElement("div");
-cover.style.background = "orange";
-cover.style.width = "100px";
-cover.style.height = "100px";
-cover.style.top = "100px";
-cover.style.left = "100px";
-cover.style.position = "absolute";
-// document.body.appendChild(cover);
-function createCover(index){
+let board = document.createElement("div");
+board.style.backgroundImage = "url('https://mir-s3-cdn-cf.behance.net/project_modules/fs/184d8c69187341.5b77be3f6e0a4.jpg')";
+board.style.top = "100px";
+board.style.left = "100px";
+board.style.width = "600px";
+board.style.height = "600px";
+board.style.position = "absolute";
+document.body.appendChild(board);
+
+function createCover() {
     cover = document.createElement("div");
     cover.style.background = "orange";
-    cover.style.width = "100px";
-    cover.style.height = "100px";
-    cover.style.top = "100px";
-    cover.style.left = "100px";
+    cover.style.width = "110px";
+    cover.style.height = "110px";
+    cover.style.top = "50px";
     cover.style.position = "absolute";
     return cover;
 }
-function createLabel(index){
+function createLabel(index) {
     let label = document.createElement("div");
     label.innerText = index;
     label.style.position = "absolute";
     label.style.top = "35px";
-    label.style.left = "50px";
+    label.style.left = "45px";
     label.style.fontSize = "30px";
     return label;
 }
 
 // let label01 = createLabel(1);
 // cover.appendChild(label01);
-let khoangcach =0;
-for(let col=0;col<20;col++){
-    document.body.appendChild()
-    cover.style.left= `${khoangcach}px`;
+
+let khoangcach = 12, row = 4, totalCover = 20;
+for (let col = 1; col <= 20; col++) {
+    board.appendChild(createCover());
+    cover.style.left = `${khoangcach}px`;
     cover.appendChild(createLabel(col));
-    khoangcach +=50;
+    khoangcach += 120;
 }
 // create card
     // create image
